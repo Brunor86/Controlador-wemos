e6caa34248a04b85200add7f878c283f0dfba8fd
Brunor86 <brunorossini02@gmail.com>
2025-06-17 19:08:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from flask import render_template
-
-from flask import Flask, request
+from flask import Flask, request, render_template
 import paho.mqtt.client as mqtt
 
 app = Flask(__name__)
 
-# Datos del broker MQTT (HiveMQ Cloud o Mosquitto en local)
+# Datos del broker MQTT
 MQTT_BROKER = "10d5264920a243d0b583f5a78c15c5b4.s1.eu.hivemq.cloud"
 MQTT_PORT = 8883
 MQTT_USER = "brunorossini02@gmail.com"
@@ -15,7 +13,7 @@ MQTT_TOPIC_BASE = "comandos/"
 # Crear cliente MQTT
 client = mqtt.Client()
 client.username_pw_set(MQTT_USER, MQTT_PASS)
-client.tls_set()  # Usar TLS
+client.tls_set()
 client.connect(MQTT_BROKER, MQTT_PORT)
 client.loop_start()
 
@@ -33,10 +31,11 @@ def apagar(dispositivo):
     client.publish(MQTT_TOPIC_BASE + dispositivo, "OFF")
     return f"{dispositivo} apagado"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
-
+# 👇 esta ruta debe ir ANTES del if __name__ == "__main__"
 @app.route("/web")
 def web():
     return render_template("index.html")
 
+# 👇 esto siempre debe ir al final
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
